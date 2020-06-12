91306526272f855be67033f5b84a679c2f5f9258
Ho Van Chien <iocutien2112@gmail.com>
2020-06-12 23:36:34 +0700

    finish Footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './common/style.scss';
 // import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from './components/Footer';
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
+        <Footer />
       </div>
     </Router>
   );
